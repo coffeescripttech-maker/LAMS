f071e538bcf6ede529c4cd32fc9655ee0fe764c2
xedter20 <dextermiranda441@gmail.com>
2026-02-23 14:54:51 +0800

    fixes

--- python-scanner/index.py
+++ python-scanner/index.py
@@ -165,3 +165,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 7000))
     debug = os.environ.get('FLASK_ENV', 'development') != 'production'
     app.run(host='0.0.0.0', port=port, debug=debug)
+
+
+    # ss
